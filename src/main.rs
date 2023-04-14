021b44dc2e5898ad5ee9a22d2566f4cba98f949b
Alex Inestal <alex@wraph.io>
2023-04-14 23:32:50 +0100

    ADD PoC with gpt-4 interacting with a very simple API that is able to SAVE or GET data. For now just fake the API and see if the AI interacts with it, which it does.

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+mod chat_context;
+mod chat_gpt;
+
+use anyhow::Result;
+use chat_gpt::ChatGPT;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Getting the API key here
+    let key = std::env::var("OPENAI_API_KEY")?;
+
+    // Creating a new ChatGPT client.
+    // Note that it requires an API key, and uses
+    // tokens from your OpenAI API account balance.
+    let mut gpt = ChatGPT::new(key)?;
+
+    // Let's chat! Initial setup
+    let input = "You are connected to an API that will receive your response and treat it as its request.
+    This API will then return a response that you can use to know the result of your request.
+    This is a simple API that will give you some storage capabilities, you can store some text and retrieve it later.
+    Your response needs to be in JSON and have the following structure:
+    {
+        \"command\": SAVE | GET,
+        \"data\": string,
+    }
+    The command will depend on what you want to do, and the data parameter will depend
+    on the command you want to execute. For example, if you want to save a text, you
+    will need to provide the text you want to save as the data parameter. If you want to
+    retrieve a text, you will need to provide the id of the text you want to retrieve
+    as the data parameter.
+    ".to_string();
+
+    let response = gpt.completion(input.clone()).await?;
+    println!("Response from GPT: {}", response.to_string());
+    gpt.context.push(response);
+
+    // Response to GPT
+    let input = "Response from the API:
+    {
+        \"status\": \"success\",
+        \"id\": \"1\"
+    }
+
+    You can now send a new request to the API.
+    "
+    .to_string();
+    let response = gpt.completion(input.clone()).await?;
+    println!("Response from GPT: {}", response);
+    gpt.context.push(response);
+
+    // Response to GPT
+    let input = "Response from the API:
+    {
+        \"status\": \"success\",
+        \"id\": \"1\",
+        \"data\": \"This is a sample text to save into storage.\"
+    }
+
+    You can now send a new request to the API.
+    "
+    .to_string();
+    let response = gpt.completion(input.clone()).await?;
+    println!("Response from GPT: {}", response);
+    gpt.context.push(response);
+
+    println!("Context: {:?}", gpt.context);
+
+    Ok(())
+}
